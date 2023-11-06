30d2fa8867c937e53d65a75e1c57310ee4a2f445
IF <ivfv@yahoo.com>
2023-11-06 23:47:59 +0100

    added navbar which i kinda like for now, but it needs to be upgraded

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+
 const Navbar=()=>{
     return (    
-                <div className='navBar'>
-                    <div>
-                        <Link to="/">Home</Link>
+                <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+                
+                    <ul className="flex justify-center space-x-4">
+                        
+                       <div>
+                        <Link to="/" className='text-white text-xs '>Home</Link>
                     </div>
                     <div>
-                        <Link to="/about">about</Link>
+                        <Link to="/about" className='text-white text-xs '>Login</Link>
                 </div>
-                    <div >
-                        <Link to="/A">A</Link>
-                    </div>
+                    
+                    </ul>
+                   
                 </div>
     )
 
--- client/src/layout/layout.jsx
+++ client/src/layout/layout.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import '../index.css';
 
 
 // Importing all created components
 import Navbar from "../components/Navbar";
+import Test from "../components/test";
 
 // Pass the child props
 export default function Layout({ children }) {
   return (
     <div>
       {/* Attaching all file components */}
-      <Navbar />
+      <Test />
       {children}
     </div>
   );

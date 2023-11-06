1b1ed86d721ab128988dab567e6d4259935eed5c
IF <ivfv@yahoo.com>
2023-11-06 23:52:37 +0100

    added navbar which i kinda like for now, but it needs to be upgraded

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import {Link} from 'react-router-dom';
 
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
 
-const Navbar=()=>{
-    return (    
-                <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-                
-                    <ul className="flex justify-center space-x-4">
-                        
-                       <div>
-                        <Link to="/" className='text-white text-xs '>Home</Link>
-                    </div>
-                    <div>
-                        <Link to="/about" className='text-white text-xs '>Login</Link>
-                </div>
-                    
-                    </ul>
-                   
-                </div>
-    )
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
-}
+  return (
+    <div className='flex justify-between items-center h-12 max-w-[1240px] mx-auto px-4 text-white bg-black'>
+      <h1 className='w-full text-3xl font-bold'>WG</h1>
+      <ul className='hidden md:flex'>
+        <li className='p-4'>
+        <Link to="/" className=''>Home</Link>
+        </li>
+        <li className='p-4'>
+            <Link to="/about" className=''>About</Link>
+        </li>
+        <li className='p-4'>
+        <Link to="/login" className=''>Login</Link>
+        </li>
+        
+      </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+      </div>
+      <ul className={nav ? 'fixed left-0 top-12 w-[60%] max-h-[30%] border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        
+          <li className='p-4 border-b border-gray-600'>
+          <Link to="/" className=''>Home</Link>
+          </li>
+          <li className='p-4 border-b border-gray-600'>
+          <Link to="/about" className=''>About</Link>
+          </li>
+          <li className='p-4 border-b border-gray-600'>
+          <Link to="/login" className=''>Login</Link>
+          </li>
+        
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;

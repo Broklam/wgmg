d7037e94f963de044201d46dc4ff8fedfc181d37
IF <ivfv@yahoo.com>
2023-11-08 15:39:36 +0100

    started how

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
           <h2 className="w-9/12 font-normal">
             It's easy to plan and follow chores with your neighbours.
           </h2>
-          <button className="border border-black bg-white px-4 py-2 rounded-full font-medium active:scale-90 transition duration-100">
+          <button className="border border-black bg-white px-4 py-2 rounded-full font-medium active:scale-90 transition duration-100 animate-fade-up animate-once animate-duration-[1500ms] animate-delay-500 animate-ease-linear">
             How?    
           </button>
         </div>
--- client/src/components/How.jsx
+++ client/src/components/How.jsx
@@ -2,7 +2,40 @@ import React from 'react'
 
 const How = () => {
   return (
-    <div>How</div>
+    <>
+    <div className="flex justify-between items-center bg-black py-10 lg:py-0 border-y border-black xl:border-hidden xl:rounded-xl min-h-[920px]">
+      <div className='grid grid-cols-4 gap-4'>
+        <ul className='text-[#ffc017] text-6xl md:text-7xl max-w-xl font-serif w-11/12 sm:w-9/12'>1.Create your account
+        <img
+          className="hidden sm:inline-flex h-40 lg:h-80 xl:h-full"
+          src="./account.jpg"
+          alt=""
+        /></ul>
+        
+        <ul className='text-[#ffc017] text-6xl md:text-7xl max-w-xl font-serif w-11/12 sm:w-9/12'>2.Make your schedule
+        <img
+          className="hidden sm:inline-flex h-40 lg:h-80 xl:h-full"
+          src="./account.jpg"
+          alt=""
+        />
+        </ul>
+        <ul className='text-[#ffc017] text-6xl md:text-7xl max-w-xl font-serif w-11/12 sm:w-9/12 animate-shake animate-duration-[1500ms] animate-delay-500 animate-ease-in'>2.Make your schedule
+        <img
+          className="hidden sm:inline-flex h-40 lg:h-80 xl:h-full"
+          src="./account.jpg"
+          alt=""
+        />
+        </ul>
+        <ul className='text-[#ffc017] text-6xl md:text-7xl max-w-xl font-serif w-11/12 sm:w-9/12'>2.Make your schedule
+        <img
+          className="hidden sm:inline-flex h-40 lg:h-80 xl:h-full"
+          src="./account.jpg"
+          alt=""
+        />
+        </ul>
+      </div>
+    </div>
+    </>
   )
 }
 
